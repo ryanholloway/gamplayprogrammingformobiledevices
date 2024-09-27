be028832a5bd664bbbdb1788b926b4cac9ad6b5a
Ryan Holloway <c00283423@setu.ie>
2024-09-27 10:24:16 +0100

    lab 3, finds variables but not variables in scope

---
+++ Lab 3/Lab 3/Lab 3.cpp
@@ -0,0 +1,64 @@
+#include "Lab3.h"
+#include <vector>
+int main()
+{
+	//vector only used to automate test cases
+	vector<char*> testCases;
+	char code1[1000] = "int main() \n{ \n int vVar1 =10;\n int var_2 =10;\n if(vVar1<10) \n {\n int var3=20;\n} \n}\n";
+	char code2[1000] = "int main() \n{ \n int a = 1;\n char b = 'c'; \n int x, y;\n int z=5;\n}\n";
+	char code3[1000] = "int init() \n{ \n char first = 'A'; \n char second = 'B';\n int num = 100;\n}\n";
+	char code4[1000] = "char test() \n{ \n int alpha = 9;\n int beta;\n if(alpha == 9)\n {\n char gamma = 'G';\n }\n}\n";
+	char code5[1000] = "int calc() \n{ \n int a = 0;\n int result = 100;\n while(result > 0)\n {\n int temp = result;\n result--;\n }\n}\n";
+	char code6[1000] = "void process() \n{ \n char flag = 'Y';\n int count = 10;\n if(flag == 'Y') \n {\n char status = 'N';\n }\n}\n";
+	char code7[1000] = "void example() \n{ \n int counter = 0;\n if(counter == 0)\n {\n int step = 1;\n }\n char letter = 'Z';\n}\n";
+	char code8[1000] = "int test() \n{ \n int var1;\n char var2;\n if(var1 == 1)\n {\n int innerVar = 20;\n }\n}\n";
+	
+	testCases.push_back(code1);
+	testCases.push_back(code2);
+	testCases.push_back(code3);
+	testCases.push_back(code4);
+	testCases.push_back(code5);
+	testCases.push_back(code6);
+	testCases.push_back(code7);
+	testCases.push_back(code8);
+	
+	//chose testcase
+	int num = 0;
+	cout << "Chose test case\n1. All Variables\n2. All Variables In Scope\n";
+	while (num < 1 || num>2)
+	{
+		cin >> num;
+	}
+	
+
+	for (auto test: testCases)
+	{
+		char variables[1000] = "";
+		switch (num)
+		{
+		case 1:
+			FindAllVariables(variables, test);
+			break;
+		case 2:
+			FindAllVariablesInScope(variables, test, 3);
+			break;
+		default:
+			break;
+		}
+		int codeIndex = 0;
+		while (variables[codeIndex] != '\0')
+		{
+			cout << variables[codeIndex];
+			codeIndex++;
+		}
+		cout << "\n\n\n";
+	}
+	
+	
+	
+	
+
+}
+
+
+
---
+++ Lab 3/Lab 3/Lab3.h
@@ -0,0 +1,77 @@
+#pragma once
+#include <iostream>
+using namespace std;
+void FindAllVariables(char variables[1000], char code[1000])
+{
+	int codeIndex = 0;
+	int variableIndex = 0;
+	bool inFunction = false;
+	while (code[codeIndex]!='\0')
+	{
+		if (code[codeIndex] == '{')
+		{
+			inFunction = true;
+		}
+		if (inFunction)
+		{
+			if ((code[codeIndex] == 'i' && code[codeIndex + 1] == 'n' && code[codeIndex + 2] == 't') || (code[codeIndex] == 'c' && code[codeIndex + 1] == 'h' && code[codeIndex + 2] == 'a' && code[codeIndex + 3] == 'r'))
+			{
+				codeIndex += (code[codeIndex] == 'i') ? 4 : 5; // skip distance based on whether int or char
+
+				while (code[codeIndex] == ' ' || code[codeIndex] == '\t') //skipping whitespace
+				{
+					codeIndex++;
+				}
+
+				while (code[codeIndex] >= 'a' && code[codeIndex] <= 'z' || code[codeIndex] >= 'A' && code[codeIndex] <= 'Z' || code[codeIndex] >= '0' && code[codeIndex] <= '9' || code[codeIndex] == '_')
+				{
+					variables[variableIndex] = code[codeIndex];
+					variableIndex++;
+					codeIndex++;
+				}
+				variables[variableIndex] = ',';
+				variableIndex++;
+
+			}
+		}
+		codeIndex++;
+	}
+}
+void FindAllVariablesInScope(char variables[1000], char code[1000], int lineNumber)
+{
+	
+	int codeIndex = 0;
+	int variableIndex = 0;
+	bool inFunction = false;
+	int countLines = 0;
+	while (code[codeIndex] != '\0')
+	{
+		if (code[codeIndex] = '\n')
+		{
+			countLines++;
+		}
+		if (countLines >= lineNumber)
+		{
+			if ((code[codeIndex] == 'i' && code[codeIndex + 1] == 'n' && code[codeIndex + 2] == 't') || (code[codeIndex] == 'c' && code[codeIndex + 1] == 'h' && code[codeIndex + 2] == 'a' && code[codeIndex + 3] == 'r'))
+			{
+				codeIndex += (code[codeIndex] == 'i') ? 4 : 5; // skip distance based on whether int or char
+
+				while (code[codeIndex] == ' ' || code[codeIndex] == '\t') //skipping whitespace
+				{
+					codeIndex++;
+				}
+
+				while (code[codeIndex] >= 'a' && code[codeIndex] <= 'z' || code[codeIndex] >= 'A' && code[codeIndex] <= 'Z' || code[codeIndex] >= '0' && code[codeIndex] <= '9' || code[codeIndex] == '_')
+				{
+					variables[variableIndex] = code[codeIndex];
+					variableIndex++;
+					codeIndex++;
+				}
+				variables[variableIndex] = ',';
+				variableIndex++;
+
+			}
+		}
+		codeIndex++;
+	}
+}
